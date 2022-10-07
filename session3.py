94823637238317c5b0ef61936a6d23cab3adfc04
MUHAMMADFAHRAZ <114728476+MUHAMMADFAHRAZ@users.noreply.github.com>
2022-10-07 14:27:16 +0700

    Add files via upload

---
+++ session3.py
@@ -0,0 +1,21 @@
+#program created by muhamad fahraz firdaus
+#ti22i class
+
+#input data
+name = "fahraz"
+kelas = "ti22i"
+major = "informatics engineering"
+age = 19
+quote = "believe that life is worth living and your belief will help create the fact."
+ilikebaso = True
+hobbies = ["read novel", "watch anime", "play football"]
+
+#display
+print("hello ! my name is :", name)
+print("i'm a student of :", kelas)
+print('majoring :', major)
+print("i'm", age, "years old")
+print("my motto :", quote)
+print("do i like baso?", ilikebaso)
+print("my hobbies are :", hobbies)
+
